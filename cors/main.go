19d65037d1bc163c7e9905cfbcae4232bbd668e1
hg6p <hg6p@users.noreply.github.com>
2024-06-24 09:42:04 +0200

    cors problem solved

--- cors/main.go
+++ cors/main.go
@@ -17,6 +17,10 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%v\n%v", w, r)
+	w.Header().Set("Access-Control-Allow-Origin", "*") // not a real fix for cors
+	// more of quick fix
+	// cors should be set to specific origin where is your production running
+	// but this is on localhost so it's does not matter but it's really important for production
 	io.WriteString(w, "hello world!")
 
 }
