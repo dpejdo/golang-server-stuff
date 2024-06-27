c793c8282e65a577bcea36cef6f8b3a3a0342725
hg6p <hg6p@users.noreply.github.com>
2024-06-27 13:25:58 +0200

    xml response

--- server-response/main.go
+++ server-response/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", writeJson)
+	http.HandleFunc("/", writeXml)
 	http.ListenAndServe(":3000", nil)
 	fmt.Print("listening on port 3000")
 
@@ -18,14 +18,14 @@ type Profile struct {
 	Hobbies []string
 }
 
-func writeJson(w http.ResponseWriter, r *http.Request) {
+func writeXml(w http.ResponseWriter, r *http.Request) {
 	profile := Profile{Name: "john", Hobbies: []string{"reading", "coding"}}
 
-	js, err := json.Marshal(profile)
+	js, err := xml.MarshalIndent(profile, "", " ")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-type", "application/xml")
 	w.Write(js)
 }
