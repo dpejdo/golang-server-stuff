b3aa1e24f142b06489734f8cd16eeee69edd9bd4
hg6p <hg6p@users.noreply.github.com>
2024-07-01 21:44:08 +0200

    mux with closure as handlerFunction

--- servemuxes/main.go
+++ servemuxes/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+func timeHandler(t string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tm := time.Now().Format(t)
+		w.Write([]byte("Time is: " + tm))
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	rh := http.RedirectHandler("http://example.org", 307)
-
-	mux.Handle("/foo", rh)
-
+	mux.HandleFunc("/time", timeHandler(time.RFC1123))
 	log.Print("Listening ....")
 	http.ListenAndServe(":3000", mux)
 }
