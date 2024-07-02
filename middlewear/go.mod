8ec9bb4b4657f6b8102468466ca302cf043cd144
hg6p <hg6p@users.noreply.github.com>
2024-07-02 22:47:55 +0200

    Add 3rd party logger middlewear

--- middlewear/go.mod
+++ middlewear/go.mod
@@ -2,4 +2,9 @@ module middlewear
 
 go 1.22.4
 
-require github.com/goji/httpauth v0.0.0-20160601135302-2da839ab0f4d // indirect
+require github.com/goji/httpauth v0.0.0-20160601135302-2da839ab0f4d
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
+)
--- middlewear/main.go
+++ middlewear/main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/goji/httpauth"
+	"github.com/gorilla/handlers"
 )
+
+func newLogginHandler(dst io.Writer) func(http http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+
+		return handlers.LoggingHandler(dst, h)
+	}
+}
 
 func final(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
 func main() {
-	authHandler := httpauth.SimpleBasicAuth("alice", "password")
+	logFile, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0664)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	loggerHandler := newLogginHandler(logFile)
 	mux := http.NewServeMux()
 
 	finalHandler := http.HandlerFunc(final)
-	mux.Handle("/", authHandler(finalHandler))
+	mux.Handle("/", loggerHandler(finalHandler))
 
 	log.Print("Listening on :3000...")
-	err := http.ListenAndServe(":3000", mux)
+	err = http.ListenAndServe(":3000", mux)
 	log.Fatal(err)
 }
