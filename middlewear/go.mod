a9662c57d1809ad7e3e65daddd87cd919c922629
hg6p <hg6p@users.noreply.github.com>
2024-07-02 22:37:18 +0200

    trying out goji/httpauth

---
+++ middlewear/go.mod
@@ -0,0 +1,5 @@
+module middlewear
+
+go 1.22.4
+
+require github.com/goji/httpauth v0.0.0-20160601135302-2da839ab0f4d // indirect
--- middlewear/main.go
+++ middlewear/main.go
@@ -2,40 +2,21 @@ package main
 
 import (
 	"log"
-	"mime"
 	"net/http"
+
+	"github.com/goji/httpauth"
 )
-
-func enforceJSONHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		contentType := r.Header.Get("Content-Type")
-
-		if contentType != "" {
-			mt, _, err := mime.ParseMediaType(contentType)
-			if err != nil {
-				http.Error(w, "Malformed Content-Type header", http.StatusBadRequest)
-				return
-			}
-
-			if mt != "application/json" {
-				http.Error(w, "Content-Type header must be application/json", http.StatusUnsupportedMediaType)
-				return
-			}
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func final(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
 func main() {
+	authHandler := httpauth.SimpleBasicAuth("alice", "password")
 	mux := http.NewServeMux()
 
 	finalHandler := http.HandlerFunc(final)
-	mux.Handle("/", enforceJSONHandler(finalHandler))
+	mux.Handle("/", authHandler(finalHandler))
 
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", mux)
