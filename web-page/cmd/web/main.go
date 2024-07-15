87016fa0d32614d1e70f0ef8244d137af23278a0
hg6p <hg6p@users.noreply.github.com>
2024-07-15 21:34:37 +0200

    add connection to database and models

--- web-page/cmd/web/main.go
+++ web-page/cmd/web/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,6 +18,11 @@ func main() {
 	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB("web:pass@tcp(localhost:3306)/snippetbox?parseTime=true")
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{infoLog: *infoLogger, errorLog: *errorLogger}
 	srv := &http.Server{
 		Addr:     ":3000",
@@ -23,6 +31,17 @@ func main() {
 	}
 
 	app.infoLog.Print("listening on port 3000")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	app.errorLog.Print(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
