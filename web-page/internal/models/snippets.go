792e7815b5325384f8f1858e1bdc507bb6d7b106
hg6p <hg6p@users.noreply.github.com>
2024-07-15 22:21:11 +0200

    Add insert function and update handler

--- web-page/internal/models/snippets.go
+++ web-page/internal/models/snippets.go
@@ -18,7 +18,20 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+
+	result, err := m.DB.Exec(
+		`INSERT INTO snippets (title, content, created, expires) VALUES (
+		?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
