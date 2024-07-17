082f1322c5283845e5b1a57b810e952f9f75be75
hg6p <hg6p@users.noreply.github.com>
2024-07-17 19:15:18 +0200

    add latest snippets query

--- web-page/cmd/web/handler.go
+++ web-page/cmd/web/handler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -17,7 +16,17 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	files := []string{"./ui/html/base.html", "ui/html/pages/home.html", "ui/html/partials/nav.html"}
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
+	/* files := []string{"./ui/html/base.html", "ui/html/pages/home.html", "ui/html/partials/nav.html"}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.errorLog.Print(err.Error())
@@ -28,7 +37,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		app.errorLog.Print(err)
 		app.serverError(w, err)
-	}
+	} */
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
