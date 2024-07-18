96cebb5ef463de952c9b53e1f15bb5157f0ef6eb
hg6p <hg6p@users.noreply.github.com>
2024-07-18 20:38:55 +0200

    Add automatic parsing for partials templates

--- web-page/cmd/web/templates.go
+++ web-page/cmd/web/templates.go
@@ -24,17 +24,19 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.html",
-			"./ui/html/partials/nav.html",
-			page,
+		ts, err := template.ParseFiles("./ui/html/base.html")
+		if err != nil {
+			return nil, err
 		}
-
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
 		if err != nil {
 			return nil, err
 		}
 
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
 		cache[name] = ts
 
 	}
